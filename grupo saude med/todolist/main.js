21a7be8b60fbe913c7a06ac5335a8c7e505673dc
jrxsousa <96900111+jrxsousa@users.noreply.github.com>
2022-01-18 08:28:01 -0300

    Delete grupo saude med/todolist directory

--- grupo saude med/todolist/main.js
+++
@@ -1,7 +0,0 @@
-import { carregaTarefa } from "./Componentes/carregaTarefa.js";
-import { handleNovoItem } from "./Componentes/criaTarefa.js";
-
-const novaTarefa = document.querySelector ('[data-form-button]');
-novaTarefa.addEventListener('click', handleNovoItem);
-
-carregaTarefa();
